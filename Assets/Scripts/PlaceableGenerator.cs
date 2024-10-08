95fae9949802ca86e58ac94e1d7b39163b69a1dd
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2024-10-07 18:03:08 -0600

    lab1

--- Assets/Scripts/PlaceableGenerator.cs
+++ Assets/Scripts/PlaceableGenerator.cs
@@ -28,6 +28,7 @@ public class PlaceableGenerator : MonoBehaviour
         {
             PlaceRandomPlaceableOnClick();
         }
+       
     }
 
     private void PlaceRandomPlaceableOnClick()
@@ -42,19 +43,24 @@ public class PlaceableGenerator : MonoBehaviour
     private void PlaceRandomPlaceable(Vector3 position)
     {
         //Pick a random placeable and Instantiate it inside the placeable container
-        
+        Instantiate(placeableObjects[Random.Range(0, placeableObjects.Length)],placeableContainer.transform,true);
     }
 
     private void UpdatePlaceableDictionary()
     {
         //Iterate through the array of placeable objects and add them to the dictionary
-        
+        foreach(PlaceableObject placeableObject in placeableObjects)
+        {
+            _placeableDictionary.Add(placeableObject.Type, placeableObject);
+           // Debug.Log("you have"+ placeableObject.name);
+        }
     }
 
     public void GeneratePlaceable(string type, Vector3 position, Vector3 rotation = default)
     {
         //Get the placeable based on the type from the dictionary if available, and instantiate the correct placeable object with the correct position and rotation values
         //and place it inside the placeable container
-        
+        Instantiate(_placeableDictionary[type], transform.position, transform.rotation);
+        Debug.Log("generate");
     }
 }
--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SaveLoadManager : MonoBehaviour
 {
@@ -12,19 +13,23 @@ public class SaveLoadManager : MonoBehaviour
     private void Save()
     {
         // Create a save data object
-        
-        
+        SaveData myObject = new SaveData();
+
         // Create an array of placeable objects
-        
+        Placeable[] placeables;
 
         // Loop through the children of the placeable container
-        
-        
+        //foreach (Placeable placeable in placeables)
+        //{
+
+        //}
+
+
         //set the array in the save data object
-        
-        
+        string saveName = JsonUtility.ToJson(myObject);
+
         // Save the save data object in playerprefs
-        
+        PlayerPrefs.SetString(saveName, _saveName);
         
         _isLoaded = true;
         Debug.Log("Data Saved");
@@ -34,12 +39,18 @@ public class SaveLoadManager : MonoBehaviour
     {
         // If data is not loaded, load the data
         if(_isLoaded) return;
-        
+
         // Get the save data from playerprefs
-        
-        
+        PlayerPrefs.GetString(_saveName);
+
         // Loop through the placeables and generate them
-        
+        JsonUtility.FromJson<Type>(_saveName);
+        placeableGenerator = new PlaceableGenerator();
+       // placeableGenerator.GeneratePlaceable(type, position, rotation);
+        //foreach (Placeable placeObject in placeables)
+        //{
+            
+        //}
         
         _isLoaded = true;
         Debug.Log("Data Loaded");
