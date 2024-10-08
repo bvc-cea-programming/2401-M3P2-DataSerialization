82099a39a8d53d0bafb9f90145d893461f1fb511
Kyle.C <k.chudobiak271@mybvc.ca>
2024-10-07 18:46:49 -0600

    Lab completed

--- Assets/Scripts/PlaceableGenerator.cs
+++ Assets/Scripts/PlaceableGenerator.cs
@@ -42,19 +42,23 @@ public class PlaceableGenerator : MonoBehaviour
     private void PlaceRandomPlaceable(Vector3 position)
     {
         //Pick a random placeable and Instantiate it inside the placeable container
-        
+        Instantiate(placeableObjects[Random.Range(0, placeableObjects.Length)], position, Quaternion.identity, placeableContainer);
     }
 
     private void UpdatePlaceableDictionary()
     {
         //Iterate through the array of placeable objects and add them to the dictionary
-        
+        foreach(var obj in placeableObjects)
+        {
+            _placeableDictionary.Add(obj.Type, obj);
+            Debug.Log(_placeableDictionary[obj.Type]);
+        }
     }
 
     public void GeneratePlaceable(string type, Vector3 position, Vector3 rotation = default)
     {
         //Get the placeable based on the type from the dictionary if available, and instantiate the correct placeable object with the correct position and rotation values
         //and place it inside the placeable container
-        
+        Instantiate(_placeableDictionary[type], position, Quaternion.Euler(rotation), placeableContainer);
     }
 }
--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -8,24 +8,34 @@ public class SaveLoadManager : MonoBehaviour
     [SerializeField] private PlaceableGenerator placeableGenerator;
     private bool _isLoaded = false;
     private string _saveName = "MAIN_SAVE";
-
+    [SerializeField] GameObject placeableContainer;
     private void Save()
     {
         // Create a save data object
-        
-        
+        SaveData saveData = new SaveData();
+
         // Create an array of placeable objects
-        
+        Placeable[] placeableObjArray = new Placeable[placeableContainer.transform.childCount];
 
         // Loop through the children of the placeable container
+        for (int i = 0; i < placeableContainer.transform.childCount; i++)
+        {
+            var placeableObj = new Placeable();
+            placeableObj.position = placeableContainer.transform.GetChild(i).gameObject.transform.position;
+            placeableObj.rotation = new Vector3(placeableContainer.transform.GetChild(i).gameObject.transform.rotation.x, placeableContainer.transform.GetChild(i).gameObject.transform.rotation.y, placeableContainer.transform.GetChild(i).gameObject.transform.rotation.z);
+            placeableObj.type = placeableContainer.transform.GetChild(i).gameObject.GetComponent<PlaceableObject>().Type;
+            placeableObjArray[i] = placeableObj;
+        }
         
-        
+        saveData.placeableArray = placeableObjArray;
+
+        Debug.Log(saveData.placeableArray.Length);
         //set the array in the save data object
-        
-        
+        var json = JsonUtility.ToJson(saveData);
+        Debug.Log(json);
+        PlayerPrefs.SetString(_saveName, json);
+
         // Save the save data object in playerprefs
-        
-        
         _isLoaded = true;
         Debug.Log("Data Saved");
     }
@@ -34,13 +44,17 @@ public class SaveLoadManager : MonoBehaviour
     {
         // If data is not loaded, load the data
         if(_isLoaded) return;
-        
+
         // Get the save data from playerprefs
-        
-        
+        var json = PlayerPrefs.GetString(_saveName);
+        SaveData tempSave = JsonUtility.FromJson<SaveData>(json);
+
         // Loop through the placeables and generate them
-        
-        
+        for (int i = 0; i < tempSave.placeableArray.Length; i++)
+        {
+            placeableGenerator.GeneratePlaceable(tempSave.placeableArray[i].type, tempSave.placeableArray[i].position, tempSave.placeableArray[i].rotation);
+        }
+
         _isLoaded = true;
         Debug.Log("Data Loaded");
     }
