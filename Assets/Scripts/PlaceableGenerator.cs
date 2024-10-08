80b20f350c07756bf6ac9bff60c8e57fa1733045
Jahnavi Patel <j.patel717@mybvc.ca>
2024-10-08 06:08:38 +0530

    Lab Activity

--- Assets/Scripts/PlaceableGenerator.cs
+++ Assets/Scripts/PlaceableGenerator.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlaceableGenerator : MonoBehaviour
 {
-    
-    [SerializeField] private PlaceableObject[] placeableObjects;
+    public GameObject[] placeableObjects; // Array of placeable objects
+    public Transform placeableContainer; // Container where the objects will be instantiated
+
+    private Dictionary<string, GameObject> placeableDictionary;
+
     [SerializeField] private LayerMask placementLayerMask;
-    [SerializeField] private Transform placeableContainer;
-    
+
+
     private Camera _mainCamera;
     private Dictionary<string, PlaceableObject> _placeableDictionary = new Dictionary<string, PlaceableObject>();
 
-    public Transform PlaceableContainer => placeableContainer;
-    
+    public Transform PlaceableContainer;
+
     private void Start()
     {
         _mainCamera = Camera.main;
@@ -22,8 +25,8 @@ public class PlaceableGenerator : MonoBehaviour
 
     private void Update()
     {
-        if(placeableObjects.Length == 0) return;
-        
+        if (placeableObjects.Length == 0) ;
+
         if (Input.GetMouseButtonDown(0))
         {
             PlaceRandomPlaceableOnClick();
@@ -42,19 +45,19 @@ public class PlaceableGenerator : MonoBehaviour
     private void PlaceRandomPlaceable(Vector3 position)
     {
         //Pick a random placeable and Instantiate it inside the placeable container
-        
+
     }
 
     private void UpdatePlaceableDictionary()
     {
         //Iterate through the array of placeable objects and add them to the dictionary
-        
+
     }
 
     public void GeneratePlaceable(string type, Vector3 position, Vector3 rotation = default)
     {
         //Get the placeable based on the type from the dictionary if available, and instantiate the correct placeable object with the correct position and rotation values
         //and place it inside the placeable container
-        
+
     }
 }
--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
@@ -8,24 +9,48 @@ public class SaveLoadManager : MonoBehaviour
     [SerializeField] private PlaceableGenerator placeableGenerator;
     private bool _isLoaded = false;
     private string _saveName = "MAIN_SAVE";
-
+    private Type _type;
+    private string type;
+    private Vector3 position;
+    private Vector3 rotation;
+    private Vector3 scale;
+    private object placeableContainer;
+    private bool placeablesArray;
+    private object mySave;
     private void Save()
     {
         // Create a save data object
-        
-        
+        SaveData saveData = new SaveData();
+        Placeable[] placeableArrayManager = new Placeable[transform.childCount];
+
         // Create an array of placeable objects
-        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            placeableArrayManager[i] = new Placeable();
+            placeableArrayManager[i].position = transform.GetChild(i).gameObject.transform.position;
+            placeableArrayManager[i].rotation = new Vector3(transform.GetChild(i).gameObject.transform.rotation.x, transform.GetChild(i).gameObject.transform.rotation.y, transform.GetChild(i).gameObject.transform.rotation.z);
+            placeableArrayManager[i].type = transform.GetChild(i).gameObject.GetComponent<PlaceableObject>().Type;
+        }
 
         // Loop through the children of the placeable container
-        
-        
+
+        for (int i = 0; i < placeableGenerator.PlaceableContainer.childCount; i++)
+        {
+            Transform child = placeableGenerator.PlaceableContainer.GetChild(i);
+            Placeable placeCurrent = new();
+            {
+                type = child.GetComponent<PlaceableObject>().Type;
+                position = child.position;
+                rotation = child.rotation.eulerAngles;
+            }
+            placeablesArray[i] = placeCurrent;
+        }
+
         //set the array in the save data object
-        
-        
-        // Save the save data object in playerprefs
-        
-        
+        mySave.placeableTypes = placeablesArray;
+
+        // Save the save data object in playerprefs PlayerPrefs.SetString(_saveName, JsonUtility.ToJson(mySave)); PlayerPrefs.SetString(_saveName, JsonUtility.ToJson(mySave));
+
         _isLoaded = true;
         Debug.Log("Data Saved");
     }
@@ -33,14 +58,18 @@ public class SaveLoadManager : MonoBehaviour
     private void Load()
     {
         // If data is not loaded, load the data
-        if(_isLoaded) return;
-        
+        if (_isLoaded) return;
+
         // Get the save data from playerprefs
-        
-        
+        string saveString = PlayerPrefs.GetString(_saveName);
+        SaveData loadTemp = JsonUtility.FromJson<SaveData>(saveString);
         // Loop through the placeables and generate them
-        
-        
+
+        foreach (Placeable p in loadTemp.placeableTypes)
+        {
+            placeableGenerator.GeneratePlaceable(type, position, rotation);
+        }
+
         _isLoaded = true;
         Debug.Log("Data Loaded");
     }
@@ -51,7 +80,7 @@ public class SaveLoadManager : MonoBehaviour
         {
             Save();
         }
-        
+
         if (Input.GetKeyDown(KeyCode.F9))
         {
             Load();
